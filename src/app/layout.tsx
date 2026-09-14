be7730fc2030e597ab7cc92ef9b216c863b6c769
Guy Mishan <73220144+GuyMishan@users.noreply.github.com>
2026-09-14 14:15:03 +0300

    Load mobile responsive styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Heebo } from "next/font/google";
 import "./globals.css";
+import "./mobile.css";
 
 const heebo = Heebo({ subsets: ["hebrew", "latin"], variable: "--font-heebo" });
 
