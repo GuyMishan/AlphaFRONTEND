dff4c4bf2e85538f69a99a2674114c5b1310822b
Guy Mishan <73220144+GuyMishan@users.noreply.github.com>
2026-09-26 16:39:20 +0300

    Import billing pricing type

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -35,6 +35,7 @@ import type {
   BillingCustomerRow,
   BillingAccountPricingProfile,
   BillingAccountPricingInput,
+  BillingAccountPricingType,
   SelfServiceBillingPricing,
   BillingPlan,
   BillingPlanInput,
